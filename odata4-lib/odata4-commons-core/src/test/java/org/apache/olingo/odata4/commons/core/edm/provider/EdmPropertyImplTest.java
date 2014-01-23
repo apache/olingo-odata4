ae46250b03d0d96d2a41764c3e07d526a7800654
Klaus Straubinger <klaus.straubinger@sap.com>
2014-01-23 13:55:32 +0100

    [OLINGO-62] more tests for EDM providers
    Change-Id: I95bc3475385d4124c3251837ed763bb82ff5b63a
    
    Signed-off-by: Christian Amend <chrisam@apache.org>

--- odata4-lib/odata4-commons-core/src/test/java/org/apache/olingo/odata4/commons/core/edm/provider/EdmPropertyImplTest.java
+++ odata4-lib/odata4-commons-core/src/test/java/org/apache/olingo/odata4/commons/core/edm/provider/EdmPropertyImplTest.java
@@ -22,9 +22,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.apache.olingo.odata4.commons.api.edm.EdmException;
 import org.apache.olingo.odata4.commons.api.edm.EdmPrimitiveType;
 import org.apache.olingo.odata4.commons.api.edm.EdmProperty;
 import org.apache.olingo.odata4.commons.api.edm.EdmType;
@@ -40,89 +42,114 @@ import org.junit.Test;
 
 public class EdmPropertyImplTest {
 
-    public void getTypeReturnsPrimitiveType() {
-        EdmProviderImpl edm = new EdmProviderImpl(mock(EdmProvider.class));
-        Property propertyProvider = new Property();
-        propertyProvider.setType(EdmPrimitiveTypeKind.Binary.getFullQualifiedName());
-        final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
-        assertTrue(property.isPrimitive());
-        final EdmType type = property.getType();
-        assertEquals(EdmTypeKind.PRIMITIVE, type.getKind());
-        assertEquals(EdmPrimitiveType.EDM_NAMESPACE, type.getNamespace());
-        assertEquals(EdmPrimitiveTypeKind.Binary.toString(), type.getName());
-       }
+  @Test
+  public void getTypeReturnsPrimitiveType() {
+    EdmProviderImpl edm = new EdmProviderImpl(mock(EdmProvider.class));
+    Property propertyProvider = new Property();
+    propertyProvider.setType(EdmPrimitiveTypeKind.Binary.getFullQualifiedName());
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    assertTrue(property.isPrimitive());
+    final EdmType type = property.getType();
+    assertEquals(EdmTypeKind.PRIMITIVE, type.getKind());
+    assertEquals(EdmPrimitiveType.EDM_NAMESPACE, type.getNamespace());
+    assertEquals(EdmPrimitiveTypeKind.Binary.toString(), type.getName());
+  }
 
-    public void getTypeReturnsComplexType() throws Exception {
-        EdmProvider provider = mock(EdmProvider.class);
-        EdmProviderImpl edm = new EdmProviderImpl(provider);
-        final FullQualifiedName complexTypeName = new FullQualifiedName("ns", "complex");
-        ComplexType complexTypeProvider = new ComplexType();
-        when(provider.getComplexType(complexTypeName)).thenReturn(complexTypeProvider);
-        Property propertyProvider = new Property();
-        propertyProvider.setType(complexTypeName);
-        final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
-        assertFalse(property.isCollection());
-        assertFalse(property.isPrimitive());
-        final EdmType type = property.getType();
-        assertEquals(EdmTypeKind.COMPLEX, type.getKind());
-        assertEquals("ns", type.getNamespace());
-        assertEquals("complex", type.getName());
-       }
-    @Test
-    public void getTypeReturnsEnumType() throws Exception {
-      EdmProvider provider = mock(EdmProvider.class);
-      EdmProviderImpl edm = new EdmProviderImpl(provider);
-      final FullQualifiedName enumTypeName = new FullQualifiedName("ns", "enum");
-      EnumType enumTypeProvider = new EnumType();
-      when(provider.getEnumType(enumTypeName)).thenReturn(enumTypeProvider);
-      Property propertyProvider = new Property();
-      propertyProvider.setType(enumTypeName);
-      final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
-      assertFalse(property.isCollection());
-      assertFalse(property.isPrimitive());
-      final EdmType type = property.getType();
-      assertEquals(EdmTypeKind.ENUM, type.getKind());
-      assertEquals("ns", type.getNamespace());
-      assertEquals("enum", type.getName());
-    }
-  
-    @Test
-    public void getTypeReturnsTypeDefinition() throws Exception {
-      EdmProvider provider = mock(EdmProvider.class);
-      EdmProviderImpl edm = new EdmProviderImpl(provider);
-      final FullQualifiedName typeName = new FullQualifiedName("ns", "definition");
-      TypeDefinition typeProvider = new TypeDefinition().setUnderlyingType(new FullQualifiedName("Edm", "String"));
-      when(provider.getTypeDefinition(typeName)).thenReturn(typeProvider);
-      Property propertyProvider = new Property();
-      propertyProvider.setType(typeName);
-      final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
-      assertFalse(property.isPrimitive());
-      final EdmType type = property.getType();
-      assertEquals(EdmTypeKind.DEFINITION, type.getKind());
-      assertEquals("ns", type.getNamespace());
-      assertEquals("definition", type.getName());
-    }
-  
-    @Test
-    public void facets() {
-      EdmProviderImpl edm = new EdmProviderImpl(mock(EdmProvider.class));
-      Property propertyProvider = new Property();
-      propertyProvider.setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
-      propertyProvider.setPrecision(42);
-      propertyProvider.setScale(12);
-      propertyProvider.setMaxLength(128);
-      propertyProvider.setUnicode(true);
-      propertyProvider.setNullable(false);
-      propertyProvider.setDefaultValue("x");
-      final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
-      assertTrue(property.isPrimitive());
-      assertNull(property.getMapping());
-      assertNull(property.getMimeType());
-      assertEquals(Integer.valueOf(42), property.getPrecision());
-      assertEquals(Integer.valueOf(12), property.getScale());
-      assertEquals(Integer.valueOf(128), property.getMaxLength());
-      assertTrue(property.isUnicode());
-      assertFalse(property.isNullable());
-      assertEquals("x", property.getDefaultValue());
-    }
+  @Test
+  public void getTypeReturnsComplexType() throws Exception {
+    EdmProvider provider = mock(EdmProvider.class);
+    EdmProviderImpl edm = new EdmProviderImpl(provider);
+    final FullQualifiedName complexTypeName = new FullQualifiedName("ns", "complex");
+    ComplexType complexTypeProvider = new ComplexType();
+    when(provider.getComplexType(complexTypeName)).thenReturn(complexTypeProvider);
+    Property propertyProvider = new Property();
+    propertyProvider.setType(complexTypeName);
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    assertFalse(property.isCollection());
+    assertFalse(property.isPrimitive());
+    final EdmType type = property.getType();
+    assertEquals(EdmTypeKind.COMPLEX, type.getKind());
+    assertEquals("ns", type.getNamespace());
+    assertEquals("complex", type.getName());
+  }
+
+  @Test
+  public void getTypeReturnsEnumType() throws Exception {
+    EdmProvider provider = mock(EdmProvider.class);
+    EdmProviderImpl edm = new EdmProviderImpl(provider);
+    final FullQualifiedName enumTypeName = new FullQualifiedName("ns", "enum");
+    EnumType enumTypeProvider = new EnumType();
+    when(provider.getEnumType(enumTypeName)).thenReturn(enumTypeProvider);
+    Property propertyProvider = new Property();
+    propertyProvider.setType(enumTypeName);
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    assertFalse(property.isCollection());
+    assertFalse(property.isPrimitive());
+    final EdmType type = property.getType();
+    assertEquals(EdmTypeKind.ENUM, type.getKind());
+    assertEquals("ns", type.getNamespace());
+    assertEquals("enum", type.getName());
+  }
+
+  @Test
+  public void getTypeReturnsTypeDefinition() throws Exception {
+    EdmProvider provider = mock(EdmProvider.class);
+    EdmProviderImpl edm = new EdmProviderImpl(provider);
+    final FullQualifiedName typeName = new FullQualifiedName("ns", "definition");
+    TypeDefinition typeProvider = new TypeDefinition().setUnderlyingType(new FullQualifiedName("Edm", "String"));
+    when(provider.getTypeDefinition(typeName)).thenReturn(typeProvider);
+    Property propertyProvider = new Property();
+    propertyProvider.setType(typeName);
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    assertFalse(property.isPrimitive());
+    final EdmType type = property.getType();
+    assertEquals(EdmTypeKind.DEFINITION, type.getKind());
+    assertEquals("ns", type.getNamespace());
+    assertEquals("definition", type.getName());
+  }
+
+  @Test(expected = EdmException.class)
+  public void getTypeReturnsWrongType() throws Exception {
+    EdmProvider provider = mock(EdmProvider.class);
+    EdmProviderImpl edm = new EdmProviderImpl(provider);
+    final Property propertyProvider = new Property()
+        .setType(new FullQualifiedName("ns", "wrong"));
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    property.getType();
+    fail();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void getTypeReturnsNoTypeKind() throws Exception {
+    EdmProvider provider = mock(EdmProvider.class);
+    EdmProviderImpl edm = new EdmProviderImpl(provider);
+    final Property propertyProvider = new Property()
+        .setType(new FullQualifiedName(EdmPrimitiveType.EDM_NAMESPACE, "type"));
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    property.getType();
+    fail();
+  }
+
+  @Test
+  public void facets() {
+    EdmProviderImpl edm = new EdmProviderImpl(mock(EdmProvider.class));
+    Property propertyProvider = new Property();
+    propertyProvider.setType(EdmPrimitiveTypeKind.String.getFullQualifiedName());
+    propertyProvider.setPrecision(42);
+    propertyProvider.setScale(12);
+    propertyProvider.setMaxLength(128);
+    propertyProvider.setUnicode(true);
+    propertyProvider.setNullable(false);
+    propertyProvider.setDefaultValue("x");
+    final EdmProperty property = new EdmPropertyImpl(edm, propertyProvider);
+    assertTrue(property.isPrimitive());
+    assertNull(property.getMapping());
+    assertNull(property.getMimeType());
+    assertEquals(Integer.valueOf(42), property.getPrecision());
+    assertEquals(Integer.valueOf(12), property.getScale());
+    assertEquals(Integer.valueOf(128), property.getMaxLength());
+    assertTrue(property.isUnicode());
+    assertFalse(property.isNullable());
+    assertEquals("x", property.getDefaultValue());
+  }
 }
