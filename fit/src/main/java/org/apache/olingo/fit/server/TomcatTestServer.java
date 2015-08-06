0e0b02788891115a40f05a47db54ee99d923b519
Michael Bolz <michael.bolz@sap.com>
2015-08-06 09:59:10 +0200

    [OLINGO-659] Fixed failing tests
    This reverts commit 3f95e194250b4b90596e688cb024dce30fbf4a4a.

--- fit/src/main/java/org/apache/olingo/fit/server/TomcatTestServer.java
+++ fit/src/main/java/org/apache/olingo/fit/server/TomcatTestServer.java
@@ -290,7 +290,6 @@ public class TomcatTestServer {
     private Context getContext() {
       if (baseContext == null) {
         baseContext = tomcat.addContext("/", baseDir.getAbsolutePath());
-        baseContext.setLoader(new WebappLoader(Thread.currentThread().getContextClassLoader()));
       }
       return baseContext;
     }
