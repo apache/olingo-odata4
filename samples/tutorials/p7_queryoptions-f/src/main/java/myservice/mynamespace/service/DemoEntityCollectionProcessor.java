c0e032d48505c6d50bf42f3008fdd6f120eaf619
Christian Holzer <c.holzer@sap.com>
2015-08-21 14:56:45 +0200

    [OLINGO-713] Proposal Tutorial - System Query Option $filter

---
+++ samples/tutorials/p7_queryoptions-f/src/main/java/myservice/mynamespace/service/DemoEntityCollectionProcessor.java
@@ -0,0 +1,134 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package myservice.mynamespace.service;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import myservice.mynamespace.data.Storage;
+
+import org.apache.olingo.commons.api.data.ContextURL;
+import org.apache.olingo.commons.api.data.Entity;
+import org.apache.olingo.commons.api.data.EntityCollection;
+import org.apache.olingo.commons.api.edm.EdmEntitySet;
+import org.apache.olingo.commons.api.edm.EdmEntityType;
+import org.apache.olingo.commons.api.format.ContentType;
+import org.apache.olingo.commons.api.format.ODataFormat;
+import org.apache.olingo.commons.api.http.HttpHeader;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.OData;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.ODataRequest;
+import org.apache.olingo.server.api.ODataResponse;
+import org.apache.olingo.server.api.ServiceMetadata;
+import org.apache.olingo.server.api.processor.EntityCollectionProcessor;
+import org.apache.olingo.server.api.serializer.EntityCollectionSerializerOptions;
+import org.apache.olingo.server.api.serializer.ODataSerializer;
+import org.apache.olingo.server.api.serializer.SerializerException;
+import org.apache.olingo.server.api.serializer.SerializerResult;
+import org.apache.olingo.server.api.uri.UriInfo;
+import org.apache.olingo.server.api.uri.UriResource;
+import org.apache.olingo.server.api.uri.UriResourceEntitySet;
+import org.apache.olingo.server.api.uri.queryoption.FilterOption;
+import org.apache.olingo.server.api.uri.queryoption.expression.Expression;
+import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
+
+public class DemoEntityCollectionProcessor implements EntityCollectionProcessor {
+
+	private OData odata;
+	private ServiceMetadata serviceMetadata;
+	private Storage storage;
+
+	public DemoEntityCollectionProcessor(Storage storage) {
+		this.storage = storage;
+	}
+
+	public void init(OData odata, ServiceMetadata serviceMetadata) {
+		this.odata = odata;
+		this.serviceMetadata = serviceMetadata;
+	}
+
+	public void readEntityCollection(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType responseFormat) throws ODataApplicationException, SerializerException {
+
+		// 1st: retrieve the requested EntitySet from the uriInfo (representation of the parsed URI)
+		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+		UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0); // in our example, the first segment is the EntitySet
+		EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+
+		// 2nd: fetch the data from backend for this requested EntitySetName and deliver as EntitySet
+		EntityCollection entityCollection = storage.readEntitySetData(edmEntitySet);
+		
+		// 3rd: Check if filter system query option is provided and apply the expression if necessary
+		FilterOption filterOption = uriInfo.getFilterOption();
+		if(filterOption != null) {
+			// Apply $filter system query option
+			try {
+			      List<Entity> entityList = entityCollection.getEntities();
+			      Iterator<Entity> entityIterator = entityList.iterator();
+			      
+			      // Evaluate the expression for each entity
+			      // If the expression is evaluated to "true", keep the entity otherwise remove it from the entityList
+			      while (entityIterator.hasNext()) {
+			    	  // To evaluate the the expression, create an instance of the Filter Expression Visitor and pass
+			    	  // the current entity to the constructor
+			    	  Entity currentEntity = entityIterator.next();
+			    	  Expression filterExpression = filterOption.getExpression();
+			    	  FilterExpressionVisitor expressionVisitor = new FilterExpressionVisitor(currentEntity);
+			    	  
+			    	  // Start evaluating the expression
+			    	  Object visitorResult = filterExpression.accept(expressionVisitor);
+			    	  
+			    	  // The result of the filter expression must be of type Edm.Boolean
+			    	  if(visitorResult instanceof Boolean) {
+			    		  if(visitorResult != null && Boolean.FALSE.equals(visitorResult)) {
+			    		    // The expression evaluated to false, so we have to remove the currentEntity from entityList
+			    		    entityIterator.remove();
+			    		  }
+			    	  } else {
+			    		  throw new ODataApplicationException("A filter expression must evaulate to type Edm.Boolean", 
+			    		      HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+			    	  }
+			      }
+
+			    } catch (ExpressionVisitException e) {
+			      throw new ODataApplicationException("Exception in filter evaluation",
+			          HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode(), Locale.ENGLISH);
+			    }
+		}
+		
+		// 4th: create a serializer based on the requested format (json)
+		ODataFormat format = ODataFormat.fromContentType(responseFormat);
+		ODataSerializer serializer = odata.createSerializer(format);
+
+		// and serialize the content: transform from the EntitySet object to InputStream
+		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+		ContextURL contextUrl = ContextURL.with().entitySet(edmEntitySet).build();
+
+		EntityCollectionSerializerOptions opts = EntityCollectionSerializerOptions.with().contextURL(contextUrl).build();
+		SerializerResult serializerResult = serializer.entityCollection(serviceMetadata, edmEntityType, entityCollection, opts);
+		InputStream serializedContent = serializerResult.getContent();
+
+		// 5th: configure the response object: set the body, headers and status code
+		response.setContent(serializedContent);
+		response.setStatusCode(HttpStatusCode.OK.getStatusCode());
+		response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
+	}
+}
---
+++ samples/tutorials/p7_queryoptions-f/src/main/java/myservice/mynamespace/service/FilterExpressionVisitor.java
@@ -0,0 +1,308 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package myservice.mynamespace.service;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.olingo.commons.api.data.Entity;
+import org.apache.olingo.commons.api.edm.EdmEnumType;
+import org.apache.olingo.commons.api.edm.EdmType;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.uri.UriInfoResource;
+import org.apache.olingo.server.api.uri.UriResource;
+import org.apache.olingo.server.api.uri.UriResourcePrimitiveProperty;
+import org.apache.olingo.server.api.uri.queryoption.expression.BinaryOperatorKind;
+import org.apache.olingo.server.api.uri.queryoption.expression.Expression;
+import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
+import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitor;
+import org.apache.olingo.server.api.uri.queryoption.expression.MethodKind;
+import org.apache.olingo.server.api.uri.queryoption.expression.UnaryOperatorKind;
+
+public class FilterExpressionVisitor implements ExpressionVisitor<Object> {
+
+	private Entity currentEntity;
+
+  public FilterExpressionVisitor(Entity currentEntity) {
+    this.currentEntity = currentEntity;
+	}
+
+  @Override
+  public Object visitMember(UriInfoResource member) throws ExpressionVisitException, ODataApplicationException {
+    // To keeps things simple, this tutorial allows only primitive properties.
+    // We have faith that the java type of Edm.Int32 is Integer
+    
+    final List<UriResource> uriResourceParts = member.getUriResourceParts();
+    
+    // Make sure that the resource path of the property contains only a single segment and a primitive property
+    // has been addressed. We can be sure, that the property exists because the UriParser checks if the
+    // property has been defined in service metadata document.
+    
+    if(uriResourceParts.size() == 1 && uriResourceParts.get(0) instanceof UriResourcePrimitiveProperty) {
+      UriResourcePrimitiveProperty uriResourceProperty = (UriResourcePrimitiveProperty) uriResourceParts.get(0);
+      return currentEntity.getProperty(uriResourceProperty.getProperty().getName()).getValue();
+      
+    } else {
+      throw new ODataApplicationException("Only primitive properties are implemented in filter expressions", 
+          HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+    }
+  }
+  
+  @Override
+  public Object visitLiteral(String literal) throws ExpressionVisitException, ODataApplicationException {
+    // To keep this tutorial simple, our filter expression visitor supports only Edm.Int32 and Edm.String
+    // In real world scenarios it can be difficult to guess the type of an literal.
+    // We can be sure, that the literal is a valid OData literal because the URI Parser checks 
+    // the lexicographical structure
+    
+    // String literals start and end with an single quotation mark
+    if(literal.startsWith("'") && literal.endsWith("'")) {
+      String stringLiteral = "";
+      if(literal.length() > 2) {
+        stringLiteral = literal.substring(1, literal.length() - 1);
+      }
+      
+      return stringLiteral;
+    } else {
+      // Try to convert the literal into an Java Integer
+      try {
+        return Integer.parseInt(literal);
+      } catch(NumberFormatException e) {
+        throw new ODataApplicationException("Only Edm.Int32 and Edm.String literals are implemented", 
+            HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+      }
+    }
+  }
+  
+  @Override
+  public Object visitUnaryOperator(UnaryOperatorKind operator, Object operand) 
+      throws ExpressionVisitException, ODataApplicationException {
+    // OData allows two different unary operators. We have to take care, that the type of the operand fits to
+    // operand
+    
+    if(operator == UnaryOperatorKind.NOT && operand instanceof Boolean) {
+      // 1.) boolean negation 
+      return !(Boolean) operand;
+    } else if(operator == UnaryOperatorKind.MINUS && operand instanceof Integer){
+      // 2.) arithmetic minus
+      return -(Integer) operand;
+    }
+    
+    // Operation not processed, throw an exception
+    throw new ODataApplicationException("Invalid type for unary operator", 
+        HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+  }
+
+	@Override
+	public Object visitBinaryOperator(BinaryOperatorKind operator, Object left, Object right) 
+	    throws ExpressionVisitException, ODataApplicationException {
+		
+	  // Binary Operators are split up in three different kinds. Up to the kind of the operator it can be applied 
+	  // to different types
+	  //   - Arithmetic operations like add, minus, modulo, etc. are allowed on numeric types like Edm.Int32
+	  //   - Logical operations are allowed on numeric types and also Edm.String
+	  //   - Boolean operations like and, or are allowed on Edm.Boolean
+	  // A detailed explanation can be found in OData Version 4.0 Part 2: URL Conventions 
+	  
+    switch (operator) {
+    
+    // Arithmetic operations
+    case ADD:
+      /** Fall through **/
+    case MOD:
+      /** Fall through **/
+    case MUL:
+      /** Fall through **/
+    case DIV:
+      /** Fall through **/
+    case SUB:
+      return evaluateArithmeticOperation(operator, left, right);
+
+    // Logical operations
+    case EQ:
+      /** Fall through **/
+    case NE:
+      /** Fall through **/
+    case GE:
+      /** Fall through **/
+    case GT:
+      /** Fall through **/
+    case LE:
+      /** Fall through **/
+    case LT:
+      return evaluateLogicalOperation(operator, left, right);
+      
+    // Boolean operations
+    case AND:
+      /** Fall through **/
+    case OR:
+      return evaluateBooleanOperation(operator, left, right);
+    
+    case HAS:
+      // Has operation is not supported. We do not use enums in our service.
+      /** fall through **/
+    default:
+      throw new ODataApplicationException("Binary operation " + operator.name() + " is not implemented", 
+          HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+    }
+  }
+
+	private Object evaluateBooleanOperation(BinaryOperatorKind operator, Object left, Object right)
+	    throws ODataApplicationException {
+	  
+	  // First check that both operands are of type Boolean
+	  if(left instanceof Boolean && right instanceof Boolean) {
+	    Boolean valueLeft = (Boolean) left;
+	    Boolean valueRight = (Boolean) right;
+	    
+	    // Than calculate the result value
+	    if(operator == BinaryOperatorKind.AND) {
+	      return valueLeft && valueRight;
+	    } else {
+	      // OR
+	      return valueLeft || valueRight;
+	    }
+	  } else {
+	    throw new ODataApplicationException("Boolean operations needs two numeric operands", 
+          HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+	  }
+	}
+
+  @SuppressWarnings("unchecked")
+  private Object evaluateLogicalOperation(BinaryOperatorKind operator, Object left, Object right) 
+      throws ODataApplicationException {
+    
+    // All types in our tutorial supports all logical operations, but we have to make sure that the types are equals
+    if(left.getClass().equals(right.getClass())) {
+      // Luckily all used types String, Boolean and also Integer supports the interface Comparable
+      // TODO: Is this OK? Otherwise we can infer generic arguments after using an if statement
+      @SuppressWarnings("rawtypes")
+      int result = ((Comparable) left).compareTo(right);
+      
+      if (operator == BinaryOperatorKind.EQ) {
+        return result == 0;
+      } else if (operator == BinaryOperatorKind.NE) {
+        return result != 0;
+      } else if (operator == BinaryOperatorKind.GE) {
+        return result >= 0;
+      } else if (operator == BinaryOperatorKind.GT) {
+        return result > 0;
+      } else if (operator == BinaryOperatorKind.LE) {
+        return result <= 0;
+      } else {
+        // BinaryOperatorKind.LT
+        return result < 0;
+      }
+      
+    } else {
+      throw new ODataApplicationException("Comparision needs to equal types", 
+          HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+    }
+  }
+
+  private Object evaluateArithmeticOperation(BinaryOperatorKind operator, Object left, 
+      Object right) throws ODataApplicationException {
+
+	  // First check if the type of of both operands is numerical
+	  if(left instanceof Integer && right instanceof Integer) {
+	    Integer valueLeft = (Integer) left;
+	    Integer valueRight = (Integer) right;
+	    
+	    // Than calculate the result value
+	    if(operator == BinaryOperatorKind.ADD) {
+	      return valueLeft + valueRight;
+	    } else if(operator == BinaryOperatorKind.SUB) {
+	      return valueLeft - valueRight;
+	    } else if(operator == BinaryOperatorKind.MUL) {
+	      return valueLeft * valueRight;
+	    } else if(operator == BinaryOperatorKind.DIV) {
+	      return valueLeft / valueRight;
+	    } else {
+	      // BinaryOperatorKind,MOD
+	      return valueLeft % valueRight;
+	    }
+	  } else {
+	    throw new ODataApplicationException("Arithmetic operations needs two numeric operands", 
+	        HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+	  }
+  }
+
+  @Override
+	public Object visitMethodCall(MethodKind methodCall, List<Object> parameters) 
+	    throws ExpressionVisitException, ODataApplicationException {
+    
+    // To keep this tutorial small and simple, we implement only one method call
+    if(methodCall == MethodKind.CONTAINS) {
+      // "Contains" gets two parameters, both have to be of type String
+      // e.g. /Products?$filter=contains(Description, '1024 MB')
+      // 
+      // First the method visistMember is called, which returns the current String value of the property.
+      // After that the method visitLiteral is called with the string literal '1024 MB',
+      // which returns a String
+      //
+      // Both String values are passed to visitMethodCall.
+      if(parameters.get(0) instanceof String && parameters.get(1) instanceof String) {
+        String valueParam1 = (String) parameters.get(0);
+        String valueParam2 = (String) parameters.get(1);
+        
+        return valueParam1.contains(valueParam2);
+      } else {
+        throw new ODataApplicationException("Contains needs two parametes of type Edm.String", 
+            HttpStatusCode.BAD_REQUEST.getStatusCode(), Locale.ENGLISH);
+      }
+    } else {
+      throw new ODataApplicationException("Method call " + methodCall + " not implemented", 
+          HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+    }
+	}
+
+	@Override
+	public Object visitTypeLiteral(EdmType type) throws ExpressionVisitException, ODataApplicationException {
+	  throw new ODataApplicationException("Type literals are not implemented", 
+        HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+	}
+	
+	 @Override
+	  public Object visitAlias(String aliasName) throws ExpressionVisitException, ODataApplicationException {
+	   throw new ODataApplicationException("Aliases are not implemented", 
+         HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+	 }
+	 
+	  @Override
+	  public Object visitEnum(EdmEnumType type, List<String> enumValues) 
+	      throws ExpressionVisitException, ODataApplicationException {
+	    throw new ODataApplicationException("Enums are not implemented", 
+          HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+	  }
+
+	  @Override
+	  public Object visitLambdaExpression(String lambdaFunction, String lambdaVariable, Expression expression) 
+	      throws ExpressionVisitException, ODataApplicationException {
+	    throw new ODataApplicationException("Lamdba expressions are not implemented", 
+          HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+	  }
+
+	  @Override
+	  public Object visitLambdaReference(String variableName) 
+	      throws ExpressionVisitException, ODataApplicationException {
+	    throw new ODataApplicationException("Lamdba references are not implemented", 
+	        HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+	  }
+}
