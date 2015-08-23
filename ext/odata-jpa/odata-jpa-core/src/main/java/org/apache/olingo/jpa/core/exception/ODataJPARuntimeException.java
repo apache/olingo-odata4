503acb0c9c899299fe016ea15f7d77a8f4e96e9e
Chandan V A <chandan.v.a@sap.com>
2015-08-23 12:57:12 +0530

    [OLINGO-765] Initial version of odata-jpa-core
    Signed-off-by: Chandan V A <chandan.v.a@sap.com>

---
+++ ext/odata-jpa/odata-jpa-core/src/main/java/org/apache/olingo/jpa/core/exception/ODataJPARuntimeException.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.olingo.jpa.core.exception;
+
+import org.apache.olingo.jpa.api.exception.ODataJPAException;
+
+public class ODataJPARuntimeException extends ODataJPAException {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  private static final String DEFAULT_BUNDLE_NAME = "odata-jpa-core-exception-i18n";
+
+  public static enum MessageKeys implements MessageKey {
+    NULL_PUNIT;
+
+    @Override
+    public String getKey() {
+      return name();
+    }
+  }
+
+  public ODataJPARuntimeException(String developmentMessage, Throwable cause, MessageKey messageKey,
+      String[] parameters) {
+    super(developmentMessage, cause, messageKey, parameters);
+  }
+
+  @Override
+  protected String getBundleName() {
+    return DEFAULT_BUNDLE_NAME;
+  }
+}
---
+++ ext/odata-jpa/odata-jpa-core/src/main/java/org/apache/olingo/jpa/core/factory/ODataJPAFactoryImpl.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.olingo.jpa.core.factory;
+
+import java.util.HashMap;
+
+import org.apache.olingo.jpa.api.ODataJPAAbstractEdmProvider;
+import org.apache.olingo.jpa.api.ODataJPAContext;
+import org.apache.olingo.jpa.api.ODataJPAProcessor;
+import org.apache.olingo.jpa.api.factory.ODataJPAFactory;
+import org.apache.olingo.jpa.core.ODataJPAContextImpl;
+import org.apache.olingo.jpa.core.edm.ODataJPAEdmProvider;
+import org.apache.olingo.jpa.core.exception.ODataJPARuntimeException;
+
+public class ODataJPAFactoryImpl extends ODataJPAFactory {
+
+  private HashMap<String, ODataJPAAbstractEdmProvider> edmProviderMap =
+      new HashMap<String, ODataJPAAbstractEdmProvider>();
+
+  @Override
+  public ODataJPAAbstractEdmProvider getODataJPAEdmProvider(String pUnitName) throws ODataJPARuntimeException {
+    ODataJPAAbstractEdmProvider odataJPAEdmProvider = null;
+    if (pUnitName == null) {
+      throw new ODataJPARuntimeException("Unable to create Edm Provider", null,
+          ODataJPARuntimeException.MessageKeys.NULL_PUNIT, null);
+    } else {
+      odataJPAEdmProvider = edmProviderMap.get(pUnitName);
+      if (odataJPAEdmProvider == null) {
+        odataJPAEdmProvider = new ODataJPAEdmProvider();
+        edmProviderMap.put(pUnitName, odataJPAEdmProvider);
+      }
+    }
+
+    return odataJPAEdmProvider;
+  }
+
+  @Override
+  public ODataJPAProcessor getODataJPAProcessor(ODataJPAContext odataJPAContext) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ODataJPAContext newODataJPAContext() {
+    return new ODataJPAContextImpl();
+  }
+
+}
