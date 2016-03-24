f7c18f7445e1f205553a93da39ccf4df76f62735
Christian Amend <christian.amend@sap.com>
2016-03-24 14:50:08 +0100

    [OLINGO-905] Add system query option access to $all

--- lib/server-api/src/main/java/org/apache/olingo/server/api/uri/UriInfoAll.java
+++ lib/server-api/src/main/java/org/apache/olingo/server/api/uri/UriInfoAll.java
@@ -18,10 +18,54 @@
  */
 package org.apache.olingo.server.api.uri;
 
+import java.util.List;
+
+import org.apache.olingo.server.api.uri.queryoption.CountOption;
+import org.apache.olingo.server.api.uri.queryoption.CustomQueryOption;
+import org.apache.olingo.server.api.uri.queryoption.FormatOption;
+import org.apache.olingo.server.api.uri.queryoption.SearchOption;
+import org.apache.olingo.server.api.uri.queryoption.SkipOption;
+import org.apache.olingo.server.api.uri.queryoption.SkipTokenOption;
+import org.apache.olingo.server.api.uri.queryoption.TopOption;
+
 /**
  * Used for URI info kind {@link UriInfoKind#all} to describe URIs like
  * http://.../serviceroot/$all
  */
 public interface UriInfoAll {
-  // No additional methods needed for now.
+  
+  /**
+   * @return List of custom query options used in the URI (without alias definitions)
+   */
+  List<CustomQueryOption> getCustomQueryOptions();
+
+  /**
+   * @return Object containing information of the $format option
+   */
+  FormatOption getFormatOption();
+
+  /**
+   * @return Object containing information of the $count option
+   */
+  CountOption getCountOption();
+
+  /**
+   * @return Object containing information of the $search option
+   */
+  SearchOption getSearchOption();
+
+  /**
+   * @return Object containing information of the $skip option
+   */
+  SkipOption getSkipOption();
+
+  /**
+   * @return Object containing information of the $skiptoken option
+   */
+  SkipTokenOption getSkipTokenOption();
+
+  /**
+   * @return Object containing information of the $top option
+   */
+  TopOption getTopOption();
 }
