84cc52cd84151a021c47849daee524e54a10feb2
Michael Bolz <michael.bolz@sap.com>
2016-04-21 05:57:45 +0200

    [OLINGO-925] Fixed import in tutorials

--- samples/tutorials/p11_batch/src/main/java/myservice/mynamespace/web/DemoServlet.java
+++ samples/tutorials/p11_batch/src/main/java/myservice/mynamespace/web/DemoServlet.java
@@ -37,7 +37,7 @@ import myservice.mynamespace.service.DemoPrimitiveProcessor;
 import org.apache.olingo.server.api.OData;
 import org.apache.olingo.server.api.ODataHttpHandler;
 import org.apache.olingo.server.api.ServiceMetadata;
-import org.apache.olingo.server.api.edmx.EdmxReference;
+import org.apache.olingo.commons.api.edmx.EdmxReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
