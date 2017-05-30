8a226e858f8f29a18a04490299bf31aa6167deb1
Dennis Kieselhorst <deki@apache.org>
2017-05-30 16:14:03 +0200

    [OLINGO-278] Update to latest version of HttpClient removed @NotThreadSafe annotation due to HTTPCLIENT-1743, @Contract(threading = ThreadingBehavior.UNSAFE) is now default

--- lib/client-core/src/main/java/org/apache/olingo/client/core/http/HttpMerge.java
+++ lib/client-core/src/main/java/org/apache/olingo/client/core/http/HttpMerge.java
@@ -20,13 +20,11 @@ package org.apache.olingo.client.core.http;
 
 import java.net.URI;
 
-import org.apache.http.annotation.NotThreadSafe;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 
 /**
  * Class identifying MERGE HTTP method.
  */
-@NotThreadSafe
 public class HttpMerge extends HttpEntityEnclosingRequestBase {
 
   public final static String METHOD_NAME = "MERGE";
--- lib/client-core/src/main/java/org/apache/olingo/client/core/http/HttpPatch.java
+++ lib/client-core/src/main/java/org/apache/olingo/client/core/http/HttpPatch.java
@@ -20,13 +20,11 @@ package org.apache.olingo.client.core.http;
 
 import java.net.URI;
 
-import org.apache.http.annotation.NotThreadSafe;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 
 /**
  * Class identifying PATCH HTTP method.
  */
-@NotThreadSafe
 public class HttpPatch extends HttpEntityEnclosingRequestBase {
 
   public final static String METHOD_NAME = "PATCH";
