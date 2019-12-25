cb6a7ce34624b7819adca3f4918bf9713f55ec0c
mibo <mibo@apache.org>
2019-12-25 18:11:14 +0100

    [OLINGO-1418] Set version to next SNAPSHOT

--- ext/karaf/karaf-fit/src/test/java/org/apache/olingo/osgi/itests/server/CarServiceTest.java
+++ ext/karaf/karaf-fit/src/test/java/org/apache/olingo/osgi/itests/server/CarServiceTest.java
@@ -96,7 +96,7 @@ public class CarServiceTest extends OlingoOSGiTestSupport {
         return new Option[] {
             olingoBaseConfig(),
             features(olingoUrl, "olingo-server", "olingo-client"),
-            mavenBundle("org.apache.olingo", "odata-server-osgi-sample", "4.7.1"),
+            mavenBundle("org.apache.olingo", "odata-server-osgi-sample", "4.8.0-SNAPSHOT"),
             logLevel(LogLevel.INFO)
         };
     }
