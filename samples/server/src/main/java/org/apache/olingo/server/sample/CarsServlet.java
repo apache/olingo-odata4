92e4a59d75c4575923c6cc55b57748e702ab7e09
Raupp Pereira <raupppee@emea.roche.com>
2023-05-30 13:53:08 +0200

    fix mockito

--- samples/server/src/main/java/org/apache/olingo/server/sample/CarsServlet.java
+++ samples/server/src/main/java/org/apache/olingo/server/sample/CarsServlet.java
@@ -21,11 +21,11 @@ package org.apache.olingo.server.sample;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import org.apache.olingo.commons.api.edmx.EdmxReference;
 import org.apache.olingo.server.api.OData;
