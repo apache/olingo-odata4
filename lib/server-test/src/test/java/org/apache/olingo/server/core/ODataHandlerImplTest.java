6cb4082ea4ae4c2eba30791d61881f551d15545e
Raupp Pereira <raupppee@emea.roche.com>
2023-05-30 12:49:58 +0200

    upgrade mockito

--- lib/server-test/src/test/java/org/apache/olingo/server/core/ODataHandlerImplTest.java
+++ lib/server-test/src/test/java/org/apache/olingo/server/core/ODataHandlerImplTest.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
